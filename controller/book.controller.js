0b40cca40e70eae10af5f2428c326ae4f4aaa92f
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-10-27 22:02:29 +0530

    Added daily-top-100 route

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -9,7 +9,7 @@ const newReleases = async (req, res) => {
         const newReleases = await Book.find({ 'Year-Of-Publication': 2000 })
             .skip(skip)
             .limit(PAGE_SIZE);
-        console.log(newReleases);
+        // console.log(newReleases);
         res.status(200).json(newReleases);
     } catch (error) {
         console.error("Error:", error);
@@ -17,6 +17,53 @@ const newReleases = async (req, res) => {
     }
 }
 
-export {
-    newReleases
+const dailyTop100 = async (req, res) => {
+    const PAGE_SIZE = 10;
+    try {
+        const page = parseInt(req.query.page) || 1;// Get the requested page from the query parameter
+        const skip = (page - 1) * PAGE_SIZE; // Calculate the number of documents to skip
+        // Sort books by ratings in descending order and limit to the top 100
+        const topBooks = await Book.aggregate([
+            {
+                $lookup: {
+                    from: 'ratings', // Assuming your ratings collection is named 'ratings'
+                    localField: 'ISBN',
+                    foreignField: 'ISBN',
+                    as: 'ratings',
+                },
+            },
+            {
+                $unwind: '$ratings',
+            },
+            {
+                $group: {
+                    _id: '$_id',
+                    'Book-Title': { $first: '$Book-Title' },
+                    'Image-URL-M': { $first: '$Image-URL-M' },
+                    averageRating: { $avg: '$ratings.Book-Rating' },
+                },
+            },
+            {
+                $sort: { averageRating: -1 },
+            },
+            {
+                $skip: skip, // Skip a certain number of documents
+            },
+            {
+                $limit: PAGE_SIZE,
+            },
+        ]);
+
+        res.json(topBooks);
+    } catch (error) {
+        res.status(500).json({ error: `${error.message}` });
+    }
 }
+
+export {
+    newReleases,
+    dailyTop100
+}
+
+
+// Ashok Sahoo 
--- router/book.route.js
+++ router/book.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { newReleases } from "../controller/book.controller.js";
+import { newReleases, dailyTop100 } from "../controller/book.controller.js";
 const router = Router();
 
 router.get("/new-releases", newReleases);
+router.get("/daily-top-100", dailyTop100);
 
 
 export default router;
