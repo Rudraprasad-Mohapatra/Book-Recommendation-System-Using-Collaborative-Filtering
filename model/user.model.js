da74474e21a214e51cc6f446a417a770c6695719
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-11-03 23:50:13 +0530

    Building user-model

--- model/user.model.js
+++ model/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema({
     userId: {
@@ -11,9 +14,48 @@ const userSchema = new mongoose.Schema({
     },
     Password: {
         type: String,
-        required: true
+        required: [true, "password is required"],
+        select: false
+    }, fullName: {
+        type: String,
+        required: [true, 'Name is required'],
+        minLength: [5, 'Name must be atleast 5 character'],
+        maxLength: [50, 'Name should be less than 50 characters'],
+        lowercase: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        lowercase: true,
+        trim: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    avatar: {
+        public_id: {
+            type: String
+        },
+        secure_url: {
+            type: String
+        }
+    },
+    role: {
+        type: String,
+        enum: ['USER', 'ADMIN'],
+        default: 'USER'
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+    subscription: {
+        id: String,
+        status: String,
     }
+}, {
+    timestamps: true
 });
+
+
 
 const User = mongoose.model('User', userSchema);
 
