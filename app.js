388d885bfcb6eaaf05cf2021b7375299abbacd30
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-11-10 12:11:05 +0530

    Building the user registration part

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
 import bookRoutes from "./router/book.route.js"
+import userRoutes from "./router/user.route.js"
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 config();
@@ -19,6 +20,8 @@ app.use(cors({
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
 app.use(morgan("dev"));
 
 app.use("/ping", function (req, res, next) {
@@ -27,6 +30,7 @@ app.use("/ping", function (req, res, next) {
 })
 
 app.use("/api/v1/book",bookRoutes);
+app.use("/api/v1/user",userRoutes);
 
 app.all("*", (req, res) => {
     res.status(404).send("OOPS!! 404 page not found")
--- router/user.route.js
+++ router/user.route.js
@@ -1,1 +1,6 @@
 import { Router } from "express";
+import { register, } from "../controller/user.controller.js"
+const router = Router();
+router.post("/register", upload.single("avatar"), register);
+
+export default router;
