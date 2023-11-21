9c5fa44891c71a0554e94060d7264cd19eeca111
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-11-21 21:31:36 +0530

    Added register router completely

--- router/user.route.js
+++ router/user.route.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { register, } from "../controller/user.controller.js"
+import { register, } from "../controller/user.controller.js";
+import upload from "../middlewares/multer.middleware.js";
+
 const router = Router();
 router.post("/register", upload.single("avatar"), register);
 
